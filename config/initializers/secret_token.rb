1734469c157964919302bac9995c56979c4b7a2d
Hajime Furukawa <hajpoj@gmail.com>
2013-02-11 17:19:42 -0800

    init + vendor js

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KnockoutFun::Application.config.secret_token = '42db5c64ffb9d9a041c883187c5f9c613918cbb30a30f4c6c911ab302fe9fa03f1d408297ba850cd8d652f81363c70b7ec58e24b316686197ba02903a4da9d73'
